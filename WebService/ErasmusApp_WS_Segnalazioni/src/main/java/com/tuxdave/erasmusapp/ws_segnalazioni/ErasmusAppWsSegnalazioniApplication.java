07d1a6eccb81030e18fa0f0f99ac91297ebe236b
TuxDave <godinodavide@gmail.com>
2022-03-04 08:45:34 +0100

    Aggiunto il progetto del Microservizio in JAVA_Spring_Boot per le Segnalazioni

---
+++ WebService/ErasmusApp_WS_Segnalazioni/src/main/java/com/tuxdave/erasmusapp/ws_segnalazioni/ErasmusAppWsSegnalazioniApplication.java
@@ -0,0 +1,13 @@
+package com.tuxdave.erasmusapp.ws_segnalazioni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErasmusAppWsSegnalazioniApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ErasmusAppWsSegnalazioniApplication.class, args);
+    }
+
+}
---
+++ WebService/ErasmusApp_WS_Segnalazioni/src/test/java/com/tuxdave/erasmusapp/ws_segnalazioni/ErasmusAppWsSegnalazioniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuxdave.erasmusapp.ws_segnalazioni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErasmusAppWsSegnalazioniApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
