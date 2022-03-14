ad38143d2f60c0443b67703cedfa74297ceb3b7a
TuxDave <godinodavide@gmail.com>
2022-03-14 16:49:25 +0100

    caricato un file dimenticato

---
+++ WebService/ErasmusApp_WS_Segnalazioni/src/main/java/com/tuxdave/erasmusapp/ws_segnalazioni/exception/custom/BindingException.java
@@ -0,0 +1,7 @@
+package com.tuxdave.erasmusapp.ws_segnalazioni.exception.custom;
+
+public class BindingException extends Exception{
+    public BindingException(String message) {
+        super(message);
+    }
+}
